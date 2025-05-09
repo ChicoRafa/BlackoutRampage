51065ec3b912e4cf9a906ee8e61c88cd14e376fe
fransanchez <fransanchezgarcia@gmail.com>
2025-05-09 22:59:10 +0200

    Adding products in store to GameManager

--- Assets/_Data/GameLogic/GameManager/GameManager.cs
+++ Assets/_Data/GameLogic/GameManager/GameManager.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
+using _Data.Customers.Orders;
+using _Data.Products;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Available Products in the Store")]
+    public List<Product> productsInStore;
+    void Awake()
+    {
+        OrderGenerator.AvailableProducts = productsInStore;
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
