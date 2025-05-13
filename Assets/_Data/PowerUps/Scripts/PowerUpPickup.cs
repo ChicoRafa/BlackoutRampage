f32cea0fdaaa54d46f2af9ae69646c1a50a384d0
Rafael Martín <chicorafa693@gmail.com>
2025-05-13 21:16:59 +0200

    Modified Scenarios and added PowerUp logic
    -PowerUps in scriptable architecture
    -Speed PowerUp
    
    TODO: Add music PowerUp, so clients are more patient. For that we'll need a sound manager first

---
+++ Assets/_Data/PowerUps/Scripts/PowerUpPickup.cs
@@ -0,0 +1,32 @@
+using _Data.PowerUps.Scriptables;
+using UnityEngine;
+
+namespace _Data.PowerUps.Scripts
+{
+    public class PowerUpPickup : MonoBehaviour
+    {
+        [SerializeField] private PowerUpSO powerUpData;
+        [SerializeField] private Vector3 rotationSpeed = new Vector3(0f, 90f, 0f); // grados por segundo
+
+        private void Update()
+        {
+            transform.Rotate(rotationSpeed * Time.deltaTime);
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+
+            powerUpData.Activate(other.gameObject);
+            StartCoroutine(DeactivateAfterDelay(other.gameObject));
+            Destroy(gameObject);
+        }
+
+        private System.Collections.IEnumerator DeactivateAfterDelay(GameObject target)
+        {
+            yield return new WaitForSeconds(powerUpData.duration);
+            powerUpData.Deactivate(target);
+        }
+        
+        public void SetPowerUp(PowerUpSO powerUp) => powerUpData = powerUp;
+    }
+}
