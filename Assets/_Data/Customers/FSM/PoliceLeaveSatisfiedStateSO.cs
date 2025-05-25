b02370fab9b552f78ff2892a6c3c05ed7c54490e
fransanchez <fransanchezgarcia@gmail.com>
2025-05-25 15:44:49 +0200

    Fixing heart floating icon bug with police leaving

--- Assets/_Data/Customers/FSM/PoliceLeaveSatisfiedStateSO.cs
+++ Assets/_Data/Customers/FSM/PoliceLeaveSatisfiedStateSO.cs
@@ -7,8 +7,8 @@ namespace _Data.Customers.FSM {
         [SerializeField] private int bonusHappiness = 25;
 
         public override void OnEnter(Client client) {
-            client.AddPoliceHappinessBonus(bonusHappiness);
             base.OnEnter(client);
+            client.AddPoliceHappinessBonus(bonusHappiness);
         }
     }
 }
