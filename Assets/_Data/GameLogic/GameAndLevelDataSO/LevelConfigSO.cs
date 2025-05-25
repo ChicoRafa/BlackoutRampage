15512bda094c328e812ae33068078f199d696359
fransanchez <fransanchezgarcia@gmail.com>
2025-05-25 21:55:10 +0200

    more adjustments to gameplay

--- Assets/_Data/GameLogic/GameAndLevelDataSO/LevelConfigSO.cs
+++ Assets/_Data/GameLogic/GameAndLevelDataSO/LevelConfigSO.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 public class LevelConfigSO : ScriptableObject
 {
     public float levelBasePatienceMultiplier = 1.0f;
-    public float patienceMultiplierForKarenExplosion = 0.5f;
-    public float speedMultiplierForLightingPowerUpTaken = 1.2f;
     public int happinessObjective = 0;
     public int moneyObjective = 0;
 }
